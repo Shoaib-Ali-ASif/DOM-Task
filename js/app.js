566b2090e56af45e802e03b7ef5b1bf20330a3af
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-07 22:53:09 +0500

    hlo

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 const lightBulb = document.getElementById("lighbulb");
-const button = document.getElementById("btn-1");
+const buttonElement = document.getElementById("btn-1");
 
-button.addEventListener("click", () => {
+buttonElement.addEventListener("click", () => {
   if (lightBulb.src.endsWith("bulboff.gif")) {
     lightBulb.src = "./pic_bulbon.gif";
-    button.textContent = "TURN OFF";
+    buttonElement.textContent = "TURN OFF";
   } else {
     lightBulb.src = "./pic_bulboff.gif";
-    button.textContent = "TURN ON";
+    buttonElement.textContent = "TURN ON";
   }
 });
 
+const divElement = document.getElementById("name");
+const btnElement = document.getElementById("btn-2");
+divElement.innerText = "Shoaib";
+btnElement.addEventListener("click" , () =>{
+  divElement.classList.toggle("hidden");
 
-const divElement = document.getElementById("div");
-const btnElement = document.getElementById("btn-3");
-btnElement.addEventListener("click", function () {
-  divElement.innerHTML += "<div>Task Complete</div>";
+});
+
+
+
+
+const div2Element = document.getElementById("div-1");
+const btn3Element = document.getElementById("btn-3");
+btn3Element.addEventListener("click", function () {
+  div2Element.innerHTML += "<div>Task Complete</div>";
 });
