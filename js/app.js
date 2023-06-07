5597a2a744b55b2268d9763862b3c0414ee221e2
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-07 23:00:35 +0500

    changed multiple things

--- js/app.js
+++ js/app.js
@@ -5,22 +5,26 @@ buttonElement.addEventListener("click", () => {
   if (lightBulb.src.endsWith("bulboff.gif")) {
     lightBulb.src = "./pic_bulbon.gif";
     buttonElement.textContent = "TURN OFF";
+    buttonElement.style.backgroundColor = "yellow";
   } else {
     lightBulb.src = "./pic_bulboff.gif";
     buttonElement.textContent = "TURN ON";
+    buttonElement.style.backgroundColor = "dodgerblue";
   }
 });
 
 const divElement = document.getElementById("name");
 const btnElement = document.getElementById("btn-2");
-divElement.innerText = "Shoaib";
-btnElement.addEventListener("click" , () =>{
-  divElement.classList.toggle("hidden");
+divElement.innerText = "Shoaib Ali Asif";
+divElement.style.display = "none";
 
+btnElement.addEventListener("click", () => {
+  if (divElement.style.display === "none") {
+    divElement.style.display = "block";
+  } else {
+    divElement.style.display = "none";
+  }
 });
-
-
-
 
 const div2Element = document.getElementById("div-1");
 const btn3Element = document.getElementById("btn-3");
