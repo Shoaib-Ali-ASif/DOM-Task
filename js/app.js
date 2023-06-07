bab2c531610ef952efab36956fb6c6796121dd0d
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-07 18:10:21 +0500

    changed multiple things

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-// app.js
+const lightBulb = document.getElementById("lighbulb");
+const button = document.getElementById("btn");
 
-// Get references to the light bulb image and the button
-const lightBulb = document.getElementById('lighbulb');
-const button = document.getElementById('btn');
-
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-  // Check the current source of the light bulb image
-  if (lightBulb.src.endsWith('bulboff.gif')) {
-    // If the image source ends with 'bulboff.gif', change it to 'bulbon.gif'
-    lightBulb.src = './pic_bulbon.gif';
+button.addEventListener("click", function () {
+  if (lightBulb.src.endsWith("bulboff.gif")) {
+    lightBulb.src = "./pic_bulbon.gif";
+    button.textContent = "TURN OFF";
   } else {
-    // Otherwise, change it back to 'bulboff.gif'
-    lightBulb.src = './pic_bulboff.gif';
+    lightBulb.src = "./pic_bulboff.gif";
+    button.textContent = "TURN ON";
   }
 });
